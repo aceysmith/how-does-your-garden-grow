d2d5b1d3588ee6a26581e92075aa09705d3f1ef1
Adam Christopher Smith <aceysmith@gmail.com>
2023-02-03 18:30:40 -0800

    Initial commit

---
+++ how-does-your-garden-grow/how-does-your-garden-grow/GameViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GameViewController.swift
+//  how-does-your-garden-grow
+//
+//  Created by Adam Christopher Smith on 2/3/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.frame.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+#if DEBUG
+            view.showsFPS = true
+            view.showsNodeCount = true
+#endif
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
